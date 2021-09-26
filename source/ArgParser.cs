c3eefc5dd26a81ebf7de3fc385af5704b893519c
Maxim <maxim@smspower.org>
2021-09-26 21:26:29 +0100

    Command like argument parser to enable printing help messages, fixes #21

---
+++ source/ArgParser.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BMP2Tile
+{
+    internal class ArgParser
+    {
+        private readonly Action<string> _extraParameterHandler;
+        private readonly Func<IList<IList<string>>> _helpTextExtraHandler;
+
+        public ArgParser(Action<string> extraParameterHandler, Func<IList<IList<string>>> helpTextExtraHandler)
+        {
+            _extraParameterHandler = extraParameterHandler;
+            _helpTextExtraHandler = helpTextExtraHandler;
+        }
+
+        private class ArgHandler
+        {
+            public IList<string> Names { get; set; }
+            public string Description { get; set; }
+            public Action<string> Action { get; set; }
+            public string ValueName { get; set; }
+        }
+        private readonly Dictionary<string, ArgHandler> _args = new Dictionary<string, ArgHandler>();
+
+        public void Add(IList<string> names, string description, Action<string> action, string argName = null)
+        {
+            var arg = new ArgHandler
+            {
+                Names = names,
+                Description = description,
+                Action = action,
+                ValueName = argName
+            };
+            foreach (var name in names)
+            {
+                _args.Add(name, arg);
+            }
+        }
+
+        public int Parse(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                ShowHelp();
+                return 1;
+            }
+            for (var i = 0; i < args.Length; ++i)
+            {
+                var arg = args[i];
+                if (arg.StartsWith("-"))
+                {
+                    // We remove any number of leading - or /
+                    var argName = Regex.Replace(arg, "^[-/]+", "");
+                    if (argName == "?" || argName == "h" || argName == "help" || !_args.TryGetValue(argName, out var handler))
+                    {
+                        Console.Error.WriteLine($"Unknown action {arg}");
+                        ShowHelp();
+                        return 1;
+                    }
+
+                    // We have a match, invoke the handler with the right number of args
+                    if (handler.ValueName != null)
+                    {
+                        if (i == args.Length - 1)
+                        {
+                            throw new Exception($"Missing value for action {arg} <{handler.ValueName}>");
+                        }
+
+                        handler.Action(args[++i]);
+                    }
+                    else
+                    {
+                        handler.Action(null);
+                    }
+                }
+                else
+                {
+                    _extraParameterHandler(arg);
+                }
+            }
+
+            return 0;
+        }
+
+        private static string PrintInGrid(IList<IList<string>> rows)
+        {
+            // Get the max width of each column
+            var maxCount = rows.Max(x => x.Count);
+            var widths = Enumerable
+                .Range(0, maxCount)
+                .Select(n => rows
+                    .Where(r => r.Count > n && r.Count > 1)
+                    .Max(r => r[n].Length) + 2)
+                .ToList();
+
+            // Then print each padded to the max width, except the last one
+            var sb = new StringBuilder();
+            foreach (var row in rows)
+            {
+                for (var i = 0; i < row.Count; i++)
+                {
+                    sb.Append(i == row.Count - 1 ? row[i] : row[i].PadRight(widths[i]));
+                }
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+
+        private void ShowHelp()
+        {
+            Console.Error.WriteLine("Usage: bmp2tile <input file> <-action> <-action ...>");
+            Console.Error.WriteLine("Actions take effect in order from left to right. Multiple input files can be processed this way.");
+            Console.Error.WriteLine("\nActions:");
+
+            Console.Error.WriteLine(PrintInGrid(GetArgs().ToList()));
+
+            Console.Error.WriteLine(PrintInGrid(_helpTextExtraHandler()));
+        }
+
+        private IEnumerable<IList<string>> GetArgs()
+        {
+            foreach (var arg in _args.Values)
+            {
+                if (arg.ValueName == null)
+                {
+                    yield return new[] { $"-{arg.Names[0]}", arg.Description };
+                }
+                else
+                {
+                    yield return new[] { $"-{arg.Names[0]} <{arg.ValueName}>", arg.Description };
+                }
+
+                foreach (var s in arg.Names.Skip(1))
+                {
+                    yield return new[] { $"-{s}", $"Synonym of -{arg.Names[0]}" };
+                }
+            }
+        }
+    }
+}
--- source/Program.cs
+++ source/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BMP2Tile
 {
@@ -33,102 +35,112 @@ namespace BMP2Tile
                     // This is different from "BMP2Tile Classic" which would accumulate the settings and
                     // then act on them at the end; but I think this way is better.
 
-                    // Since some take multiple parameters, we use lambdas to handle the "continuation".
-                    Action<string> nextArgHandler = null;
+                    // ReSharper disable AccessToDisposedClosure
+                    // ReSharper disable StringLiteralTypo
+                    var parser = new ArgParser(
+                        filename => converter.Filename = filename,
+                        () => GetCompressorInfo(converter));
+                    parser.Add(
+                        new[]{"loadimage"},
+                        "Load the image specified. Can be specified without the action.",
+                        s => converter.Filename = s,
+                        "filename");
+                    parser.Add(
+                        new[]{"removeduplicates", "removedupes"}, 
+                        "Remove duplicate tiles (default)", 
+                        _ => converter.RemoveDuplicates = true);
+                    parser.Add(
+                        new[]{"noremoveduplicates", "noremovedupes"}, 
+                        "Do not remove duplicate tiles", 
+                        _ => converter.RemoveDuplicates = false);
+                    parser.Add(
+                        new[]{"usemirroring", "mirror"}, 
+                        "Use tile mirroring to remove duplicates (default)", 
+                        _ => converter.UseMirroring = true);
+                    parser.Add(
+                        new[]{"nomirroring", "nomirror"}, 
+                        "Use tile mirroring to remove duplicates", 
+                        _ => converter.UseMirroring = false);
+                    parser.Add(
+                        new[]{"8x8"}, 
+                        "Treat image as 8x8 tiles (default)", 
+                        _ => converter.AdjacentBelow = false);
+                    parser.Add(
+                        new[]{"8x16"}, 
+                        "Treat image as 8x16 tiles (does not work will with duplicate removal)", 
+                        _ => converter.AdjacentBelow = true);
+                    parser.Add(
+                        new[]{"planar"}, 
+                        "Convert tiles to 4bpp planar format (e.g. for SMS, GG) (default)", 
+                        _ => converter.Chunky = false);
+                    parser.Add(
+                        new[]{"chunky"}, 
+                        "Convert tiles to 4bpp chunky format (e.g. for GBA, MD)", 
+                        _ => converter.Chunky = true);
+                    parser.Add(
+                        new[]{"tileoffset"}, 
+                        "Tile offset for first tile found (default is 0)", 
+                        x => converter.TileOffset = Convert.ToUInt32(x),
+                        "offset");
+                    parser.Add(
+                        new[] { "spritepalette" },
+                        "Set tilemap data to use sprite palette (default off)",
+                        _ => converter.UseSpritePalette = true);
+                    parser.Add(
+                        new[] { "infrontofsprites" },
+                        "Set tilemap data to be in front of sprites",
+                        _ => converter.HighPriority = true);
+                    parser.Add(
+                        new[] { "palcl123" },
+                        "Emit palette data using cl123-style constants (for SMS)",
+                        _ => converter.PaletteFormat = Palette.Formats.MasterSystemConstants);
+                    parser.Add(
+                        new[] { "smspalette", "palsms"},
+                        "Emit palette in SMS format (6bpp)",
+                        _ => converter.PaletteFormat = Palette.Formats.MasterSystem);
+                    parser.Add(
+                        new[] { "ggpalette", "palgg" },
+                        "Emit palette in GG format (12bpp)",
+                        _ => converter.PaletteFormat = Palette.Formats.GameGear);
+                    parser.Add(
+                        new[] { "fullpalette" },
+                        "Emit 16 palette entries regardless of image palette size",
+                        _ => converter.FullPalette = true);
+                    parser.Add(
+                        new[] { "minimumpalette"},
+                            "Emit only as many palette entries as needed to include values used in the image",
+                            _ => converter.FullPalette = false);
+                    parser.Add(
+                        new[] { "savetiles" },
+                        "Save tiles to the given filename. Extension selects compression.",
+                        s => converter.SaveTiles(s),
+                        "filename");
+                    parser.Add(
+                        new[] { "savetilemap" },
+                        "Save tilemap to the given filename. Extension selects compression.",
+                        s => converter.SaveTilemap(s),
+                        "filename");
+                    parser.Add(
+                        new[] { "savepalette" },
+                        "Save tilemap to the given filename. Format is controlled by other palette-related parameters.",
+                        s => converter.SavePalette(s),
+                        "filename");
+                    parser.Add(
+                        new[] { "exit" },
+                        "Exit the program. Later actions are ignored.",
+                        s => throw new Exception("Program exited"));
+                    parser.Add(
+                        new[] { "verbose", "v" },
+                        "Enable verbose logging",
+                        _ => _verbose = true);
+                    parser.Add(
+                        new[] { "quiet", "q" },
+                        "Disable verbose logging",
+                        _ => _verbose = false);
+                    // ReSharper restore StringLiteralTypo
+                    // ReSharper restore AccessToDisposedClosure
 
-                    foreach (var arg in args)
-                    {
-                        if (nextArgHandler != null)
-                        {
-                            nextArgHandler(arg);
-                            nextArgHandler = null;
-                            continue;
-                        }
-
-                        // ReSharper disable StringLiteralTypo
-                        // ReSharper disable AccessToDisposedClosure
-                        switch (arg.ToLowerInvariant())
-                        {
-                            case "-removedupes":
-                            case "-removeduplicates":
-                                converter.RemoveDuplicates = true;
-                                break;
-                            case "-noremovedupes":
-                            case "-noremoveduplicates":
-                                converter.RemoveDuplicates = false;
-                                break;
-                            case "-usemirroring":
-                            case "-mirror":
-                                converter.UseMirroring = true;
-                                break;
-                            case "-nomirror":
-                            case "-nomirroring":
-                                converter.UseMirroring = false;
-                                break;
-                            case "-8x8":
-                                converter.AdjacentBelow = false;
-                                break;
-                            case "-8x16":
-                                converter.AdjacentBelow = true;
-                                break;
-                            case "-planar":
-                                converter.Chunky = false;
-                                break;
-                            case "-chunky":
-                                converter.Chunky = true;
-                                break;
-                            case "-tileoffset":
-                                nextArgHandler = s => converter.TileOffset = Convert.ToUInt32(s);
-                                break;
-                            case "-spritepalette":
-                                converter.UseSpritePalette = true;
-                                break;
-                            case "-infrontofsprites":
-                                converter.HighPriority = true;
-                                break;
-                            case "-palcl123":
-                                converter.PaletteFormat = Palette.Formats.MasterSystemConstants;
-                                break;
-                            case "-palsms":
-                            case "-smspalette":
-                                converter.PaletteFormat = Palette.Formats.MasterSystem;
-                                break;
-                            case "-palgg":
-                            case "-ggpalette":
-                                converter.PaletteFormat = Palette.Formats.GameGear;
-                                break;
-                            case "-fullpalette":
-                                converter.FullPalette = true;
-                                break;
-                            case "-minimumpalette":
-                                converter.FullPalette = false;
-                                break;
-                            case "-savetiles":
-                                nextArgHandler = s => converter.SaveTiles(s);
-                                break;
-                            case "-savetilemap":
-                                nextArgHandler = s => converter.SaveTilemap(s);
-                                break;
-                            case "-savepalette":
-                                nextArgHandler = s => converter.SavePalette(s);
-                                break;
-                            case "-exit":
-                                return 0;
-                            case "-v":
-                            case "-verbose":
-                                _verbose = true;
-                                break;
-                            case "-q":
-                            case "-quiet":
-                                _verbose = false;
-                                break;
-                            default:
-                                converter.Filename = arg;
-                                break;
-                        }
-                        // ReSharper restore AccessToDisposedClosure // Use of converter in lambdas
-                        // ReSharper restore StringLiteralTypo // Parameter names
-                    }
+                    return parser.Parse(args);
                 }
             }
             catch (Exception ex)
@@ -140,8 +152,25 @@ namespace BMP2Tile
                 }
                 return 1;
             }
+        }
 
-            return 0;
+        private static IList<IList<string>> GetCompressorInfo(Converter converter)
+        {
+            return new[]
+                {
+                    new[] { "Available compressors:" },
+                    new[] { "Extension", "Name", "Capabilities" }
+                }
+                .Concat(
+                    converter.GetCompressorInfo()
+                        .Select(compressor => new[]
+                        {
+                            compressor.Extension,
+                            compressor.Name,
+                            compressor.Capabilities.ToString()
+                        }))
+                .Cast<IList<string>>()
+                .ToList();
         }
     }
 }
